bcfbe2c78e2bb8d9dbc3943824dde8242dbc8a6f
Kenta Hirata <kenta.hirata@gmail.com>
2025-01-14 23:22:39 +0900

    css wh -> vw

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
 $(function() {
-    // ここに追記
+// ここに追記
+});
+
+
+
+window.addEventListener('scroll', function() {
+    const suggest_img = document.querySelectorAll('.suggest_img');
+    const scroll = window.scrollY;
+    const windowHeight = window.innerHeight;
+
+    suggest_img.forEach(function(img) {
+        const distanceToImg = img.offsetTop;
+        if(scroll + windowHeight - 300 > distanceToImg) {
+            img.classList.add('is-active');
+        }
+        else {
+            img.classList.remove('is-active');
+        }
+    });
 });
