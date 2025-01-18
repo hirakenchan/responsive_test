15643a760db27721ae097003094f077fe774fc17
Kenta Hirata <kenta.hirata@gmail.com>
2025-01-19 08:43:53 +0900

    modal show No.1

--- app.js
+++ app.js
@@ -1,27 +1,36 @@
 $(function () {
     // ここに追記
-});
+    window.addEventListener('scroll', function () {
+        const suggest_img = document.querySelectorAll('.suggest_img');
+        const scroll = window.scrollY;
+        const windowHeight = window.innerHeight;
+        const header = this.document.querySelector('header');
 
-
-window.addEventListener('scroll', function () {
-    const suggest_img = document.querySelectorAll('.suggest_img');
-    const scroll = window.scrollY;
-    const windowHeight = window.innerHeight;
-    const header = this.document.querySelector('header');
-
-    if (scroll <= 0) {
-        header.classList.remove("scroll");
-    } else {
-        header.classList.add("scroll");
-    }
-
-    suggest_img.forEach(function (img) {
-        const distanceToImg = img.offsetTop;
-        if (scroll + windowHeight - 300 > distanceToImg) {
-            img.classList.add('is-active');
+        if (scroll <= 0) {
+            header.classList.remove("scroll");
+        } else {
+            header.classList.add("scroll");
         }
-        else {
-            img.classList.remove('is-active');
-        }
+
+        suggest_img.forEach(function (img) {
+            const distanceToImg = img.offsetTop;
+            if (scroll + windowHeight - 300 > distanceToImg) {
+                img.classList.add('is-active');
+            }
+            else {
+                img.classList.remove('is-active');
+            }
+        });
+    });
+
+    // const modal = document.getElementById('modal');
+    // const room_search = document.getElementById('room_search');
+    // room_search.onclick = function() {
+    //     modal.style.display = 'block';
+    // }
+
+    $('#room_search').on('click', function() {
+        $('#modal').css('display', 'block');
     });
 });
+
